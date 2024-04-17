eb2e9d4bc95770e77580c794da1532d2ca7cbffb
Mathew Zamacona <mateoszamacona@icloud.com>
2024-04-17 09:17:33 -0600

    added requirements, challenge file and test

---
+++ your_age_in_days/age.py
@@ -0,0 +1,6 @@
+#make a simple function that will calculate your age in days 
+# simply get your age and times it by 365 
+# note return a int not a str
+
+def age_in_days(age: int) -> int:
+    pass
---
+++ your_age_in_days/readme.md
@@ -0,0 +1,3 @@
+Your age in days
+
+Make a function that will calculate your age in days.
