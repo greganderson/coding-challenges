506c5cf5e1c28a4830ac7bf296917b1174f522b8
Greg Anderson <1920444+greganderson@users.noreply.github.com>
2024-05-30 09:58:06 -0600

    Update unit tests

--- remove-middle/test_remove_middle.py
+++ remove-middle/test_remove_middle.py
@@ -2,5 +2,15 @@ from main import remove_middle
 
 
 def test_remove_middle():
-    assert remove_middle([1, 2, 3, 4, 5, 6, 7]) == [1, 2, 3, 5, 6, 7]
-    assert remove_middle([1, 2, 3, 4, 5, 6]) == [1, 2, 3, 4, 5, 6]
+    start = [1, 2, 3, 4, 5, 6, 7]
+    expected = [1, 2, 3, 5, 6, 7]
+    result = remove_middle(start)
+    assert result == 4
+    assert start == expected
+
+    even = [1, 2, 3, 4, 5, 6]
+    expected = [1, 2, 3, 4, 5, 6]
+    result = remove_middle(even)
+    assert result is None
+    assert even == expected
+
