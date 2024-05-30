ebd3b1c3035c19db9db04507214bb8798551f625
Greg Anderson <1920444+greganderson@users.noreply.github.com>
2024-05-30 09:54:21 -0600

    Update remove_middle.py

--- remove-middle/remove_middle.py
+++ remove-middle/remove_middle.py
@@ -1,3 +1,3 @@
-# Remove the middle element from a list and returns the new list, if list is an even amount of items, don't remove anything.
+# Remove the middle element from a list and return it. If list has an even number of items, don't remove anything.
 def remove_middle(arr: list) -> list | None:
     pass
